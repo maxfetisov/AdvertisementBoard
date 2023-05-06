dec866e9e6d31979b7da07012ef0f011559e34b2
Maxim <maksimka.fetisov.02@mail.ru>
2023-05-06 19:45:51 +0300

    Ajax & category controller

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,16 @@
-onload = function() {
-    alert("f");
+onload = function () {
+    $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        url: "/api/categories",
+        dataType: 'json',
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+            console.log(data);
+        },
+        error: function (e) {
+            console.log(e);
+        }
+    });
 }
