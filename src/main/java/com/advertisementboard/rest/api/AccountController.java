d1b686cf2607221615257d1413b676b08eb5bfe7
Maxim <maksimka.fetisov.02@mail.ru>
2023-06-06 21:02:45 +0300

    Role from account endpoint

--- src/main/java/com/advertisementboard/rest/api/AccountController.java
+++ src/main/java/com/advertisementboard/rest/api/AccountController.java
@@ -3,25 +3,17 @@ package com.advertisementboard.rest.api;
 import com.advertisementboard.data.dto.authentication.AuthenticationRequestDto;
 import com.advertisementboard.data.dto.authentication.AuthenticationResponseDto;
 import com.advertisementboard.data.dto.authentication.RegistrationRequestDto;
+import com.advertisementboard.data.dto.role.RoleDto;
 import com.advertisementboard.data.dto.user.UserDto;
 import com.advertisementboard.data.entity.User;
 import com.advertisementboard.service.account.AccountService;
-import com.advertisementboard.service.mapper.UserMapper;
-import com.advertisementboard.service.security.JwtService;
-import com.advertisementboard.service.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,18 +24,25 @@ public class AccountController {
     private final AccountService accountService;
 
     @GetMapping
-    public ResponseEntity<UserDto> getAccount(Authentication authentication){
-        User user = (User)authentication.getPrincipal();
-        return ResponseEntity.ok(UserDto.builder().login(user.getLogin()).name(user.getName()).build());
+    public ResponseEntity<UserDto> getAccount(final Authentication authentication) {
+        final User user = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(UserDto.builder()
+                .login(user.getLogin())
+                .name(user.getName())
+                .role(RoleDto.builder()
+                        .id(user.getRole().getId())
+                        .name(user.getRole().getName())
+                        .build())
+                .build());
     }
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponseDto> register(@RequestBody final RegistrationRequestDto request){
+    public ResponseEntity<AuthenticationResponseDto> register(@RequestBody final RegistrationRequestDto request) {
         return ResponseEntity.ok(accountService.register(request));
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponseDto> authenticate(@RequestBody final AuthenticationRequestDto request){
+    public ResponseEntity<AuthenticationResponseDto> authenticate(@RequestBody final AuthenticationRequestDto request) {
         return ResponseEntity.ok(accountService.authenticate(request));
     }
 
